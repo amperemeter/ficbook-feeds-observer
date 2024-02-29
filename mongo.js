8a27ec261c89d5ecb4e7a9c9ffd826a66b3a11ff
darya <daryakushina@gmail.com>
2024-03-01 00:33:41 +0300

    mongo to core

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const  assert = require('assert'),
+  MongoClient = require('mongodb').MongoClient,
+  uri = require('./uri'),
+  Proto = require('./tools/proto'),
+  ReadCollection = require('./tools/read-collection');
+
+MongoClient.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true}, async function (err, client) {
+  assert.equal(null, err);
+  const collection = client.db('fanficsdb').collection('fanfics');
+  const fanfics = await collection.find({}).toArray();
+
+  const saveCount = async (obj) => {
+    try {
+      if (obj.difference !== 0) {
+        await collection.updateOne({url: obj.url}, {$set: {count: obj.articleCount}});
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  await ReadCollection(fanfics,  Proto, saveCount);
+  await client.close(); // закрыть подключение с БД
+});
