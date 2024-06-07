da41029c599c4e6d748e48bcc58da26963a3fb14
darya <daryakushina@gmail.com>
2024-06-07 16:49:35 +0300

    repository

--- local.js
+++ local.js
@@ -1,15 +1,20 @@
-const fs = require('file-system');
-const {readCollection} = require('./tools/read-collection');
-const fanfics = require('./data/fanfics');
+const fs = require("file-system");
+const { readCollection } = require("./tools/read-collection");
+const fanfics = require("./data/fanfics");
 
-(async () => {
+const app = async () => {
   const changedFanfics = [];
 
-  await readCollection(fanfics, {changedFanfics});
+  await readCollection(fanfics, { changedFanfics });
 
   if (fanfics.length === changedFanfics.length) {
-    await fs.writeFileSync('./data/fanfics.json', JSON.stringify(changedFanfics, null, 2));
+    await fs.writeFileSync(
+      "./data/fanfics.json",
+      JSON.stringify(changedFanfics, null, 2),
+    );
   } else {
     console.log("Ошибка. Данные не могут быть сохранены");
   }
-})();
+};
+
+module.exports = app;
